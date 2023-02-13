2f525a78347e4b58e51d09cb7da13f8367acdb4c
Jasmine Zhang <zhangirl3@gmail.com>
2023-02-13 16:52:48 -0500

    initial code

--- server.py
+++ server.py
@@ -1,23 +1,77 @@
 import socket
 
-def Main():
-   
-    host = '10.250.77.19' #Server ip
-    port = 4000
+def start_server():
+    #starting the server
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('10.250.77.19', 12345))
+    server_socket.listen(5)
+    print("Server started, waiting for clients")
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind((host, port))
+    # keep track of which clients are connected
+    client_connected = []
 
-    print("Server Started")
+    # create a dictionary called accounts to store all the accounts created by the client
+    accounts = {}
+    messages = {}
+
     while True:
-        data, addr = s.recvfrom(1024)
-        data = data.decode('utf-8')
-        print("Message from: " + str(addr))
-        print("From connected user: " + data)
-        data = data.upper()
-        print("Sending: " + data)
-        s.sendto(data.encode('utf-8'), addr)
-    c.close()
+        client_socket, client_address = server_socket.accept()
+        print(f"Connected to by: {client_address}")
+        client_socket.sendall(b"Welcome to the chat server!\n")
 
-if __name__=='__main__':
-    Main()
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+
+            data = data.decode().strip()
+            call = data.split()
+
+            if len(call) == 0:
+                continue
+
+            #create an account
+            if call[0] == "create":
+                username = call[1]
+                if username in accounts:
+                    client_socket.sendall(f"Username '{username}' is already taken\n".encode())
+                else:
+                    accounts[username] = []
+                    client_socket.sendall(f"Account '{username}' created\n".encode())
+
+            #list an accoutn
+            elif call[0] == "list":
+                client_socket.sendall("\n".join(accounts.keys()).encode() + b"\n")
+
+            elif call[0] == "login":
+                if call[1] in accounts and accounts[call[1]] == call[2]:
+                    client_socket.send("Login successful\n".encode())
+                    client_connected.append(client_socket)
+                    if call[1] in messages:
+                        for message in messages[call[1]]:
+                            client_socket.send(message.encode())
+                        messages[call[1]] = []
+                else:
+                    client_socket.send("Login failed\n".encode())
+
+            elif call[0] == "send":
+                recipient = call[1]
+                if recipient in accounts:
+                    if recipient in client_connected:
+                        accounts[recipient].sendall("Message from {}: {}\n".format(call[2], " ".join(call[3:])).encode())
+                    else:
+                        if recipient in messages:
+                            messages[recipient].append("Message from {}: {}\n".format(call[2], " ".join(call[3:])))
+                        else:
+                            messages[recipient] = ["Message from {}: {}\n".format(call[2], " ".join(call[3:]))]
+                else:
+                    client_socket.sendall("Recipient not found\n".encode())
+
+            else:
+                client_socket.sendall(b"Unknown command\n")
+
+        client_socket.close()
+        print(f"Closed connection with {client_address}")
+
+if __name__ == "__main__":
+    start_server()
